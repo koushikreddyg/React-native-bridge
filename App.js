ff6546e8da2874097611612320284acff3c36fd6
Koushik Reddy <koushikreddygunapati@Koushiks-MBP.attlocal.net>
2019-04-07 18:55:16 -0500

    react native location

--- App.js
+++ App.js
@@ -1,43 +1,91 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, requireNativeComponent, NativeModules, Platform} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, NativeModules, Platform, TouchableOpacity, AppState, DeviceEventEmitter, PermissionsAndroid } from 'react-native';
 
 
 
 export default class App extends Component {
-constructor(props) {
-  super(props);
-  this._onStatusChange = this._onStatusChange.bind(this);
-  this.state = { isOn: false };
-}
-_onStatusChange = e => {
-  this.setState({ isOn: e.nativeEvent.isOn});
-}
-render() {
-  Platform.OS==='android'? NativeModules.Counter.show(res=>console.warn(res)): console.warn(NativeModules.Counter)
-return (
-<View style={styles.container}>
-<View style={styles.top} >
-  <Text>This state of Bulb come from Native Code to JavaScript</Text>
-  <Text>{this.state.isOn ? "Bulb is On" : "Bulb is Off"}</Text>
-</View>
-{/* <Bulb style={ styles.bottom }  isOn={this.state.isOn} onStatusChange={this._onStatusChange} /> */}
-</View>
-);
-}
+  constructor(props) {
+    super(props);
+    this.state = { appState: AppState };
+  }
+  componentDidMount() {
+    NativeModules.Counter._startListen();
+    AppState.addEventListener('change', this._handleAppStateChange);
+    // this.checkLocationSettings();
+
+    DeviceEventEmitter.addListener('locationProviderStatusChange', (locationStatus) => {
+      console.warn("gps listener has chhanged", locationStatus)
+  })
+  }
+
+  openLocationSettings = () => {
+    NativeModules.Counter.openLocationSettings();
+  }
+  openGPSSettings = () => {
+    NativeModules.Counter.openGPSSettings();
+  }
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+  _handleAppStateChange = (nextAppState) => {
+    if ( nextAppState === 'active') {
+      this.setState({ appState: nextAppState });
+    } 
+    // this.checkLocationSettings();
+
+  };
+
+  checkLocationSettings = () => {
+    NativeModules.Counter.isLocationEnabled((gps)=>{
+      PermissionsAndroid
+      .check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+      .then(location=>{
+        if(!location && !gps){
+          this.openGPSSettings();
+          this.openLocationSettings();
+        }else if(!location && gps){
+          this.openLocationSettings();
+        }else if(location && !gps){
+          this.openGPSSettings();
+        }
+  
+      })
+    })
+   
+  }
+  render() {
+    // Platform.OS === 'android' ? NativeModules.Counter.show(res => console.warn(res)) : console.warn(NativeModules.Counter)
+    return (
+      <View style={styles.container}>
+        <View style={styles.top} >
+          <TouchableOpacity>
+            <Text onPress={this.openLocationSettings}>open location settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text onPress={this.openGPSSettings}>open GPS settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text onPress={this.checkLocationSettings}>check for the location</Text>
+          </TouchableOpacity>
+        </View>
+        {/* <Bulb style={ styles.bottom }  isOn={this.state.isOn} onStatusChange={this._onStatusChange} /> */}
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-backgroundColor: '#F5FCFF',
-},
-top: {
-flex: 1,
-alignItems: "center",
-justifyContent: "center",
-},
-bottom: {
-flex: 1,
-alignItems: "center",
-justifyContent: "center",
-},
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  top: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottom: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
